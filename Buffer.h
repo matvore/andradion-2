e8081959e875628d30dedc440ca08b6ec02a98f0
Matt DeVore <matvore@gmail.com>
2011-09-03 17:25:09 +0900

    Adding license and making sure modules compile
    See modified files for affected modules.

--- Buffer.h
+++ Buffer.h
@@ -1,3 +1,19 @@
+/*
+Copyright 2011 Matt DeVore
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 class Buffer {
   static inline void *allocate(unsigned int size) throw(std::bad_alloc) {
     void *result = std::malloc(size);
@@ -40,6 +56,8 @@ public:
     size = rhs.size;
 
     std::memcpy(buf, rhs.buf, size);
+
+    return *this;
   }
 
   void Reallocate(unsigned int s) throw(std::bad_alloc) {
